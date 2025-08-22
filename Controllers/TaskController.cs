c7460925b45a452a448c8f97c7a5b6baccd68dc8
Gabriel Duarte <gabriel.duarte18@outlook.es>
2025-08-21 21:33:25 -0600

    Crear una tarea

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -26,4 +26,19 @@ public class TaskController: ControllerBase
         
         return Ok(task);
     }
+
+    [HttpPost]
+    public ActionResult<Models.Task> CreateTask(Models.TaskInsert taskInsert)
+    {
+        var newTask = new Models.Task()
+        {
+            Id = TaskDataStore.Current.Tasks.Max(t => t.Id) + 1,
+            CreatedAt = DateTime.Now,
+            UpdatedAt = DateTime.Now,
+            Title = taskInsert.Description,
+        };
+        
+        TaskDataStore.Current.Tasks.Add(newTask);
+        return Ok(newTask);
+    }
 }
---
+++ Models/TaskInsert.cs
@@ -0,0 +1,7 @@
+namespace TaskMasterAPI.Models;
+
+public class TaskInsert
+{
+    public string? Title { get; set; } =  String.Empty;
+    public string? Description { get; set; } = String.Empty;
+}
