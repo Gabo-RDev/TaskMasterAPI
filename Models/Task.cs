b229eca4ec0d16925dfc0c74286f30ac445549ba
Gabriel Duarte <gabriel.duarte18@outlook.es>
2025-08-21 21:09:30 -0600

    Creación de modelo y servicio

---
+++ Models/Task.cs
@@ -0,0 +1,11 @@
+namespace TaskMasterAPI.Models;
+
+public class Task
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
